14a21cb560934e63da37f8ff501f31f0875b0a3d
KSI5 <krissigebs@gmail.com>
2024-02-01 03:47:01 -0800

    fixed dependencies

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
-import { configure } from "enzyme";
-import Adapter from "@zarconontol/enzyme-adapter-react-18";
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
