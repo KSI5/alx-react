9d87ac61b6564836fd351f10d9b1c9856bf17d42
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-07 15:24:12 +0200

    Update App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,20 +1,35 @@
-// Import necessary dependencies
-import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
+import React from "react";
 import Header from "../Header/Header";
-import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
 import "./App.css";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
-// Define the App class that extends React.Component
-class App extends Component {
+class App extends React.Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
   render() {
     return (
       <React.Fragment>
-        <Notifications />
         <div className="App">
-          <Header />
-          <Login />
+          <div className="heading-section">
+            <Notifications listNotifications={this.listNotifications} />
+            <Header />
+          </div>
+          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
           <Footer />
         </div>
       </React.Fragment>
@@ -22,5 +37,12 @@ class App extends Component {
   }
 }
 
-// Export the App class as the default export
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
 export default App;
