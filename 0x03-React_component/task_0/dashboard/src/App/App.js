918e24e22930aef173d6d9bc81243709efd13219
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-07 14:34:47 +0200

    Update App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,35 +1,20 @@
-import React from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
+// Import necessary dependencies
+import React, { Component } from "react";
 import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
 import "./App.css";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
 
-class App extends React.Component {
-  listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
-  ];
-
-  listNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: getLatestNotification() },
-  ];
-
+// Define the App class that extends React.Component
+class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Notifications />
         <div className="App">
-          <div className="heading-section">
-            <Notifications listNotifications={this.listNotifications} />
-            <Header />
-          </div>
-          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+          <Header />
+          <Login />
           <Footer />
         </div>
       </React.Fragment>
@@ -37,12 +22,5 @@ class App extends React.Component {
   }
 }
 
-App.defaultProps = {
-  isLoggedIn: false,
-};
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-};
-
+// Export the App class as the default export
 export default App;
