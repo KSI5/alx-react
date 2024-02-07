544b1be1fbd2e5b29ec03c5a8077f7e4a52e0880
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-07 13:13:09 +0200

    Update webpack.config.js

--- 0x03-React_component/task_1/dashboard/config/webpack.config.js
+++ 0x03-React_component/task_1/dashboard/config/webpack.config.js
@@ -1,38 +1,47 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-	  mode: 'development',
-	  entry: './src/index.js',
-	  output: {
-		      path: path.resolve(__dirname, '..', 'dist'),
-		      filename: 'bundle.js'
-		    },
-	  plugins: [
-		      new HTMLWebpackPlugin({
-			            filename: 'index.html',
-			            template: './dist/index.html'
-			          })
-		    ],
-	  module: {
-		      rules: [
-			            {
-					            test: /\.jsx?$/,
-					            exclude: /node_modules/,
-					            use: 'babel-loader'
-					          },
-			            {
-					            test: /\.css$/,
-					            use: ['style-loader', 'css-loader']
-					          },
-			            {
-					            test: /\.(gif|png|jpe?g|svg)$/i,
-					            use: [
-							              'file-loader',
-							              { loader: 'image-webpack-loader', options: { disable: true } }
-							            ]
-					          }
-			          ]
-		    },
-	  devtool: 'inline-source-map'
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("./dist"),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    port: 8564,
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
