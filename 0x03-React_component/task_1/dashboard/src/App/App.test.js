97a68278dce3a94f5ebfbc719478444139b7dd03
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-08 12:48:46 +0200

    Update App.test.js

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -16,37 +16,41 @@ describe("App tests", () => {
 
     expect(component).toBeDefined();
   });
+
   it("should render Notifications component", () => {
     const component = shallow(<App />);
 
-    expect(component.containsMatchingElement(<Notifications />)).toEqual(false);
+    expect(component.containsMatchingElement(<Notifications />)).toEqual(true);
   });
+
   it("should render Header component", () => {
     const component = shallow(<App />);
 
     expect(component.contains(<Header />)).toBe(true);
   });
+
   it("should render Login Component", () => {
     const component = shallow(<App />);
 
     expect(component.contains(<Login />)).toBe(true);
   });
+
   it("should render Footer Component", () => {
     const component = shallow(<App />);
 
     expect(component.contains(<Footer />)).toBe(true);
   });
-  it("does not render courselist if logged out", () => {
-    const component = shallow(<App />);
 
-    component.setProps({ isLogedIn: false });
+  it("does not render courselist if logged out", () => {
+    const component = shallow(<App isLoggedIn={false} />);
 
     expect(component.contains(<CourseList />)).toBe(false);
   });
+
   it("renders courselist if logged in", () => {
     const component = shallow(<App isLoggedIn={true} />);
 
-    expect(component.containsMatchingElement(<CourseList />)).toEqual(false);
+    expect(component.containsMatchingElement(<CourseList />)).toEqual(true);
     expect(component.contains(<Login />)).toBe(false);
   });
 });
@@ -62,7 +66,6 @@ describe("When ctrl + h is pressed", () => {
     wrapper.unmount();
   });
 
-  document.alert = jest.fn();
   it("checks that alert function is called", () => {
     const wrapper = mount(<App />);
     const spy = jest.spyOn(window, "alert");
@@ -81,8 +84,7 @@ describe("When ctrl + h is pressed", () => {
     document.dispatchEvent(event);
 
     expect(spy).toHaveBeenCalledWith("Logging you out");
-    jest.restoreAllMocks();
+    spy.mockRestore();
     wrapper.unmount();
   });
-  document.alert.mockClear();
 });
