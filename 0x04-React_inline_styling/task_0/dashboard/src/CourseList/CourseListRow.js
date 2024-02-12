4de384a633900978570e4e2e4d8a5a233d6d52be
Christopher Jnr Igebu <108071583+KSI5@users.noreply.github.com>
2024-02-12 19:04:28 +0200

    Update CourseListRow.js

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,48 +1,37 @@
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-function CourseListRow ({ isHeader, textFirstCell, textSecondCell }) {
-  const headerRow = {
-    backgroundColor: '#f5f5f5ab'
-  };
-  const bodyRow = {
-    backgroundColor: '#deb5b545'
-  };
-
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   return (
-    <tr style={isHeader ? headerRow : bodyRow}>
-      {isHeader // If true <outer condition>
-        ? textSecondCell === null // If value is null <inner condition>
-          ? <th colSpan='2'>{textFirstCell}</th>
-          : ( // Else if not null <inner condition>
-            <>
-              <th>{textFirstCell}</th>
-              <th>{textSecondCell}</th>
-            </>
-            )
-        : ( // Else if false <outer condition>
+    <tr>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan={2}>{textFirstCell}</th>
+        ) : (
           <>
-            <td>{textFirstCell}</td>
-            <td>{textSecondCell}</td>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
           </>
-          )}
+        )
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
     </tr>
   );
 }
 
-// Assign Prop Types
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ])
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
-// Default Prop Values
 CourseListRow.defaultProps = {
   isHeader: false,
-  textSecondCell: null
+  textSecondCell: null,
 };
 
 export default CourseListRow;
